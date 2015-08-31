3a71ece3b85f5cb320cf30d078e716e1a809a161
Japheth Balane <japheth162@gmail.com>
2015-08-31 14:34:40 +0800

    Line was limited to 360 degrees

--- script.js
+++ script.js
@@ -8,51 +8,42 @@ var line = [];
 
 clearCanvas();
 generateLine();
-setInterval(drawTree, 30);
+setInterval(drawLine, 30);
 
 function randomBetween(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function clearCanvas() {
-	context.fillStyle = "#aaaaaa";
+	context.fillStyle = "#ffffff";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function generateLine() {
-	line.push(new Line(0, 360));
-}
-
-function drawTree() {
-	clearCanvas();
-	for (var i = 0; i < line.length; i++) {
-		line[i].update(canvas.width/2, canvas.height/2).draw();
-		if (line[i].grow) {
-			line.push(new Line(0, 360))
-		};
+	for (var i = 0; i < 360; i++) {
+		line.push(new Line(i+1));
 	};
 }
 
-function Line(ang1, ang2) {
+function drawLine() {
+	clearCanvas();
+	for (var i = 0; i < line.length; i++) {
+		line[i].update().draw();
+	};
+}
+
+function Line(ang) {
 	this.x1 = canvas.width/2;
 	this.y1 = canvas.height/2;
 	this.x2 = canvas.width/2;
 	this.y2 = canvas.height/2;
-	this.life = 100;
-	this.grow = false;
-	this.angle = randomBetween(ang1, ang2);
-	this.speed = 3;
+	this.life = 250;
+	this.angle = ang;
 
-	this.update = function(x, y) {
-		var dx = Math.cos(this.angle * Math.PI / 180) * this.speed;
-		var dy = Math.sin(this.angle * Math.PI / 180) * this.speed;
-		this.x1 = x;
-		this.y1 = y;
+	this.update = function() {
+		var dx = Math.cos(this.angle * Math.PI / 180);
+		var dy = Math.sin(this.angle * Math.PI / 180);
 		this.life--;
-
-		if (this.life <= 0) {
-			this.grow = true;
-		};
 		if (this.life > 0) {
 			this.x2+=dx;
 			this.y2-=dy;
