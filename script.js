c4a0f89c26d5eecc2be42707da44beebf656f07b
Japheth Balane <japheth162@gmail.com>
2015-12-28 15:07:30 +0800

    Border added

--- script.js
+++ script.js
@@ -30,6 +30,11 @@ function drawLine() {
 	for (var i = 0; i < lines.length; i++) {
 		lines[i].update().draw();
 	};
+	connectCircle();
+	drawOutline();
+}
+
+function connectCircle(){
 	context.beginPath();
 	context.moveTo(lines[0].x2, lines[0].y2);
 	context.lineTo(lines[lines.length-1].x2, lines[lines.length-1].y2);
@@ -37,36 +42,47 @@ function drawLine() {
 	context.stroke();
 }
 
+function drawOutline(){
+	context.beginPath();
+	context.arc(canvas.width/2, canvas.height/2, 500 - lines[0].life, Math.PI * 2, false);
+	context.strokeStyle = "#000";
+	context.fillStyle = "rgba(100,100,100,0.3)";
+	context.stroke();
+	context.fill();
+}
+
 function Line(ang) {
 	this.x1 = canvas.width/2;
 	this.y1 = canvas.height/2;
 	this.x2 = canvas.width/2;
 	this.y2 = canvas.height/2;
-	this.life = 200;
+	this.life = 350;
 	this.angle = ang;
 
 	this.update = function() {
 		var dx = Math.cos(this.angle * Math.PI / 180);
 		var dy = Math.sin(this.angle * Math.PI / 180);
-		this.life--;
+		if (this.life > 0) {
+			this.life--;
+		};
 		if (this.life > 0) {
 			this.x2+=dx;
 			this.y2-=dy;
 		};
 
-		if (this.life <= 0) {
+		if (this.life < 100) {
 			var x = randomBetween(0,4);
 			if (x == 0) {
-				this.x2-=0.05;
+				this.x2-=0.1;
 			};
 			if (x == 1) {
-				this.x2+=0.05;
+				this.x2+=0.1;
 			};
 			if (x == 2) {
-				this.y2-=0.05;
+				this.y2-=0.1;
 			};
 			if (x == 3) {
-				this.y2+=0.05;
+				this.y2+=0.1;
 			};
 		};
 
